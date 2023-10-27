a2dec322fb7ec1df68a0f3e9777323dc9a91b4f2
Denis Basov <den4er@mail.ru>
2023-10-27 13:31:22 +0300

    основы js

---
+++ 06 JS основы/script.js
@@ -0,0 +1,123 @@
+// объявляем переменную
+// let age = 88;
+// let firstName = "Ivan";
+// let lastName = "Ivanov";
+
+// console.log(age);
+
+/*
+let cats = 5;
+let dogs = 2;
+
+cats + dogs;
+
+let animals = cats + dogs; // 7
+console.log(animals);*/
+
+/*
+let animals = 55;
+animals + 1;
+console.log(animals);// 56*/
+
+/*
+let animals = 55;
+animals = animals + 5; // = (2),  + (12)
+animals += 5;
+animals++; // инкремент - увеличение значения переменной на 1
+animals--; // декремент - уменьшение на 1
+
+console.log(animals); // 65
+*/
+
+/*
+const cats = 5;
+cats--;
+*/
+
+/*
+let greet = "Hello";
+let animal = "Elephant";
+console.log(greet + ', ' + animal);
+// конкатенация - объединение строк
+*/
+
+/*
+let firstName = "Ян";
+let lastName = "Иванов";
+let age = 33;
+
+// Привет, меня зовут Иван, моя фамилия Иванов, мне 33 года.
+
+// 1 - конкатенация
+// let message = "Привет, меня зовут " + firstName + ", моя фамилия " + lastName + ", мне " + age + " года.";
+// console.log(message);
+
+// 2 - строковые шаблоны
+let message = `Привет, меня зовут ${firstName}, моя фамилия ${lastName}, мне ${age} года.`;
+console.log(message);
+*/
+
+// 1 задание сформируйте строку из переменных
+// let catName = "Барсик";
+// let age = 3;
+// let hobby = "Спать на батарее";
+// Привет, меня зовут Барсик, моё хобби - Спать на батарее, мне 3 года.
+
+//1
+// console.log(`Привет, меня зовут ${catName}, моё хобби - ${hobby}, мне ${age} года.`);
+
+//2
+// let catMessage = `Привет, меня зовут ${catName}, моё хобби - ${hobby}, мне ${age} года.`;
+
+// console.log(catMessage);
+
+//3
+/*
+let catName = "Барсик";
+let age = 3;
+let hobby = "Спать на батарее";
+// Привет, меня зовут Барсик, моё хобби - Спать на батарее, мне 3 года.
+
+let message = `Привет, меня зовут ${catName}, моё хобби - ${hobby}, мне ${age} года.`;
+console.log(message);*/
+
+//4
+// let catName = "Рыжий котик";
+// let age = 5;
+// let hobby = "мяукать по ночам";
+// Привет, меня зовут Барсик, моё хобби - Спать на батарее, мне 3 года.
+
+// let message = `Привет, меня зовут ${catName}, моё хобби - ${hobby}, мне ${age} года.`;
+
+// console.log(message);
+
+//5
+// console.log(`Привет, меня зовут ${catName}, моё хобби - "${hobby}", мне ${age} года.`);
+
+//6
+// let catName = "Барсик";
+// let age = "3";
+// let hobby = "Спать на батарее";
+// let massage = `Привет меня зовут ${catName}, мое хобби ${hobby}, мне ${age} года`;
+// console.log(massage);
+
+/*
+let catName = "Барсик";
+let age = "3";
+let hobby = "Спать на батарее";
+let massage = `Привет меня зовут ${catName}, мое хобби ${hobby}, мне ${age} года`;
+console.log(massage);
+
+console.log(+age + 1); // 4
+*/
+
+// обращение к символам строки
+let firstName = "иван";
+// console.log(firstName[1]); // в
+
+// увечить первую букву имени
+// let firstLetter = firstName[0];
+// console.log(firstLetter.toUpperCase());
+
+let firstLetterUpper = firstName[0].toUpperCase();
+console.log(firstLetterUpper);
