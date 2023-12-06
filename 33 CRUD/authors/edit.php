7196e2ac453f9d8b032fa0a45a6b7a4a53c042ed
Denis Basov <den4er@mail.ru>
2023-12-06 10:58:33 +0300

    CRUD

--- 33 CRUD/authors/edit.php
+++ 33 CRUD/authors/edit.php
@@ -21,13 +21,13 @@ $query = "SELECT id, first_name, last_name
             ORDER BY last_name;";
 $authors = $pdo->query($query)->fetchAll();
 
-//DBConnect::d($_POST);
+
 /**
- * при выборе автора получаем данные из бд и заполняем ими форму для редактирования
  * если отправлена форма name="action" value="Редактировать"
+ * при выборе автора получаем данные из бд и заполняем ими форму для редактирования
  */
 if( isset($_POST['action']) && $_POST['action'] === 'Редактировать' ){
-    DBConnect::d($_POST);
+    //DBConnect::d($_POST);
     // 1. получаем ID автора
     $authorId = (int)$_POST['authorId'];
 
@@ -36,9 +36,72 @@ if( isset($_POST['action']) && $_POST['action'] === 'Редактировать'
                 FROM authors
                 WHERE id = $authorId;";
     $author = $pdo->query($query)->fetch();
-    DBConnect::d($author);
+    //DBConnect::d($author);
 }
 
+
+/**
+ * если надата кнопка name="action" value="Сохранить изменения"
+ * получаем данные из формы, проверяем на пустоту, записываем в БД
+ */
+if( isset($_POST['action']) && $_POST['action'] === 'Сохранить изменения' ){
+    DBConnect::d($_POST);
+    DBConnect::d($_FILES);
+
+    // обезвреживаем данные
+    $first_name = htmlspecialchars(trim($_POST['first_name']));
+    $last_name = htmlspecialchars(trim($_POST['last_name']));
+    $short_info = htmlspecialchars(trim($_POST['short_info']));
+    $biography = htmlspecialchars(trim($_POST['biography']));
+    $authorId = (int)$_POST['id'];
+    $oldAvatarPath = htmlspecialchars(trim($_POST['avatarPath']));// путь к текущей картинке автора
+
+    // получаем данные новой аватарки
+    $avatar = $_FILES['avatar'];
+
+    // проверяем данные на пустоту
+    if(!empty($first_name)
+        && !empty($last_name)
+        && !empty($short_info)
+        && !empty($biography) )
+    {// если не пусто
+
+        // проверка на наличие новой аватарки
+        if( !$avatar['size'] ){// если размер картинки 0, то ее не приложили
+            // обновляем только текстовые данные в таблице
+            $query = "UPDATE authors
+            SET first_name = ?, last_name = ?, short_info = ?, biography = ?
+            WHERE id = ?;";
+            $statement = $pdo->prepare($query);
+            $statement->execute([$first_name, $last_name, $short_info, $biography, $authorId]);
+        }else{ // если новая картинка есть
+
+            // формируем путь к новой каринке
+            $avatarPath = '/template/images/authors/'.time().'_'.$avatar['name'];
+
+            // обновляем данные в БД
+            $query = "UPDATE authors
+            SET first_name = ?, last_name = ?, short_info = ?, biography = ?, avatar = ?
+            WHERE id = ?;";
+            $statement = $pdo->prepare($query);
+            $statement->execute([$first_name, $last_name, $short_info, $biography, $avatarPath, $authorId]);
+
+            // перемещаем новую картинку
+            move_uploaded_file($avatar['tmp_name'], '..'.$avatarPath);
+
+            // удаляем старую картинку
+            if(file_exists('..'.$oldAvatarPath)){
+                unlink('..'.$oldAvatarPath);
+            }
+
+        }
+        // перенаправляем админа на admin.php
+        header('Location: ../admin.php');
+
+    }else{
+        $errorMsg = '<h2>Заполните все поля</h2>';
+    }
+}
 
 ?>
 <!doctype html>
@@ -60,8 +123,8 @@ if( isset($_POST['action']) && $_POST['action'] === 'Редактировать'
         <div class="input-data">
             <label>Выберите автора для редактирования:</label>
             <select name="authorId">
-                <?php foreach ($authors as $author):?>
-                    <option value="<?=$author['id']?>"><?=$author['first_name']?> <?=$author['last_name']?></option>
+                <?php foreach ($authors as $value):?>
+                    <option value="<?=$value['id']?>"><?=$value['first_name']?> <?=$value['last_name']?></option>
                 <?php endforeach;?>
             </select>
         </div>
@@ -80,27 +143,33 @@ if( isset($_POST['action']) && $_POST['action'] === 'Редактировать'
         <!-- first_name, last_name, short_info, biography, avatar  -->
         <div class="input-data">
             <label>Имя:</label>
-            <input type="text" name="first_name">
+            <input type="text" name="first_name" value="<?=$author['first_name'] ?? ''?>">
         </div>
 
         <div class="input-data">
             <label>Фамилия:</label>
-            <input type="text" name="last_name">
+            <input type="text" name="last_name" value="<?=$author['last_name'] ?? ''?>">
         </div>
 
         <div class="input-data">
             <label>Короткое описание:</label><br>
-            <textarea name="short_info" cols="50" rows="10"></textarea>
+            <textarea name="short_info" cols="50" rows="10"><?=$author['short_info'] ?? ''?></textarea>
         </div>
 
         <div class="input-data">
             <label>Биография:</label><br>
-            <textarea name="biography" cols="50" rows="10"></textarea>
+            <textarea name="biography" cols="50" rows="10"><?=$author['biography'] ?? ''?></textarea>
         </div>
 
         <div class="input-data">
             <label>Аватар:</label>
             <input type="file" name="avatar">
+        </div>
+
+        <!-- передаем в скрытых инпутах необходимые данные (id автора, ссылку на картинку)-->
+        <div class="input-data">
+            <input type="hidden" name="id" value="<?=$author['id'] ?? ''?>">
+            <input type="hidden" name="avatarPath" value="<?=$author['avatar'] ?? ''?>">
         </div>
 
         <div class="input-data">
